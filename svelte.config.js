4da4b103a8f3f740761020508f6575b0c57c5832
pessi-v <pessimo@posteo.net>
2024-11-01 18:05:35 +0100

    try with added config to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sveltePreprocess(),
+	preprocess: sveltePreprocess({
+		scss: true
+	}),
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
