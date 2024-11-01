569eed1f74005b900d061304a85e973826f96cdb
pessi-v <pessimo@posteo.net>
2024-11-01 15:26:15 +0100

    add scss to project, add basic html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,78 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="heroSection">
+	<div class="logo">
+		<h4 class="label">FRIENDS IN COMMON</h4>
+	</div>
+	<div class="cardButtonMain">
+		<h4 class="label">LABEL</h4>
+		<p>
+			Body text or body copy is the text forming the main content of a book, magazine, web page, or
+			any other printed or digital work. This is as card.
+		</p>
+		<button class="buttonMain">BUTTON</button>
+	</div>
+	<div class="cardButtonSecondary">
+		<h4 class="label">LABEL</h4>
+		<p>
+			Body text or body copy is the text forming the main content of a book, magazine, web page, or
+			any other printed or digital work. This is as card.
+		</p>
+		<button class="buttonSecondary">BUTTON</button>
+	</div>
+	<div class="navBar">
+		<a href="#coming" class="navLink">LINK</a>
+		<a href="#coming" class="navLink">LINK</a>
+		<a href="#coming" class="navLink">LINK</a>
+	</div>
+	<div class="heroCard">
+		<h2>Promoting Community Ownership of HOUSING</h2>
+		<p>
+			By crowdfunding, we raise money to buy property and keep it out of the market. Join our
+			mission to create housing groups where people live collectively, with secure, rent-free homes
+			under resident control.
+		</p>
+	</div>
+	<div class="section1">
+		<h4 class="label">LABEL</h4>
+		<h2>Main Title of a Reasonable Standard Length</h2>
+		<p>
+			Body text or body copy is the text forming the main content of a book, magazine, web page, or
+			any other printed or digital work. This is as a contrast to both additional components such as
+			headings, images, charts, footnotes etc. on each page, and also the pages of front matter that
+			form the introduction to a book.
+		</p>
+		<div class="section1SubFrame">
+			<img src="" alt="" />
+			<h4 class="label">LABEL</h4>
+			<p>
+				Body text or body copy is the text forming the main content of a book, magazine, web page,
+				or any other printed or digital work. This is as a contrast to both additional components
+				such as headings, images, charts, footnotes etc. on each page, and also the pages of front
+				matter that form the introduction to a book. 0123456789 € @
+			</p>
+		</div>
+	</div>
+	<div class="sectionCards">
+		<h4 class="label">LABEL</h4>
+		<div class="cardMosaic">cards go here</div>
+	</div>
+	<div class="sectionForm">
+		<h4 class="label">JOIN US</h4>
+		<h2>Big donations help a lot, small ones do too. 5e/m, hop!</h2>
+		<p>
+			Body text or body copy is the text forming the main content of a book, magazine, web page, or
+			any other printed or digital work. This is as a contrast to both additional components such as
+			headings, images, charts, footnotes etc. on each page, and also the pages of front matter that
+			form the introduction to a book.
+		</p>
+	</div>
+	<div class="section2">
+		<h4 class="label">LABEL</h4>
+		<h2>Main Title of a Reasonable Standard Length</h2>
+		<p>
+			Body text or body copy is the text forming the main content of a book, magazine, web page, or
+			any other printed or digital work. This is as a contrast to both additional components such as
+			headings, images, charts, footnotes etc. on each page, and also the pages of front matter that
+			form the introduction to a book.
+		</p>
+	</div>
+</div>
