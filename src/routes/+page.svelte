3a36bc5f47987ff45c350ad565e829f11e58f615
pessi-v <pessimo@posteo.net>
2024-11-05 14:01:49 +0100

    add card texts and small css improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 				Your contribution makes a real difference. Every small donation to the Friend’s Fund,
 				supports the creation of a a better, more secure future for all.
 			</p>
-			<button class="btn btn-primary buttonMain">DONATE</button>
+			<a class="btn btn-primary buttonMain" href="#sectionForm">DONATE</a>
 		</div>
 	</div>
 	<div class="cardButtonSecondary">
@@ -48,7 +48,7 @@
 			more secure future for all.
 		</p>
 		<div class="section1SubFrame">
-			<img src="" alt="" />
+			<img src="./image1.png" alt="" />
 			<div class="flex-column-row-gap">
 				<h4 class="label">LABEL</h4>
 				<p>
@@ -63,47 +63,130 @@
 </div>
 <div class="sectionCards" id="sectionCards">
 	<div class="flex-column-row-gap">
-		<h4 class="label">CORE IDEAS</h4>
+		<h4 class="label" style="width: 80vw; margin-left: auto;margin-right: auto;">CORE IDEAS</h4>
 		<div class="cardMosaic">
 			<div class="cardOne mosaicCard">
 				<h2>A legal contract to keep property off the market.</h2>
+				<p>
+					Each group receiving money from Friends in Common will have to make a legal contract, with
+					FIC or another organisation, that will ensure that the property will not be sold.
+				</p>
 			</div>
-			<div class="cardTwo mosaicCard"><h2>Housing Should Be Free!</h2></div>
+			<div class="cardTwo mosaicCard">
+				<h2>Housing Should Be Free!</h2>
+				<p>
+					Housing should be as free today as inhabiting a cave would have been ten thousand years
+					ago. Whatever buildings have been built, should be considered as common inheritance of
+					humanity, to be used for the greatest good. If you're paying rent to inhabit your home,
+					you're not free as the caveman was - you're in fact in a relationship similar to slavery
+					with your landlord.
+				</p>
+			</div>
 			<div class="cardThree mosaicCard">
 				<h2>Less housing stress leaves more time for struggle.</h2>
+				<p>
+					If your housing needs are covered, or are easier to cover, you'll have more capacities for
+					political work. Donating money to Friends in Common is therefore a way to support
+					political work in general.
+				</p>
 			</div>
 			<div class="cardFour mosaicCard">
 				<h2>Housing cooperatives are social hubs for a broader community.</h2>
+				<p>
+					They offer culture, space for meeting, solidarity services and temporary lodging for
+					travellers. They are neighborhood hubs.
+				</p>
 			</div>
 			<div class="cardFive mosaicCard">
 				<h2>With climate change, societal change is inevitable.</h2>
+				<p>
+					“Only a crisis - actual or perceived - produces real change. When that crisis occurs, the
+					actions that are taken depend on the ideas that are lying around. That, I believe, is our
+					basic function: to develop alternatives to existing policies, to keep them alive and
+					available until the politically impossible becomes the politically inevitable.” - Milton
+					Friedman
+				</p>
 			</div>
 			<div class="cardSix mosaicCard">
 				<h2>
 					Permanence of residence is vital for a healthy connection with the Earth and vibrant
 					cities
 				</h2>
+				<p>
+					Gentrification kills culture by disabling the creative love that the inhabitants bring to
+					their neighborhoods. In a parallel effect, only by living for a longer time close to
+					forests, fields, meadows and bodies of water can we observe their health and progression
+					through the seasons.
+				</p>
 			</div>
 			<div class="cardSeven mosaicCard">
 				<h2>Living with community is good for you.</h2>
+				<p>
+					There is so little hope for a better future amongst left networks, but hope can be
+					powerful. Being a part of a positive movement gives determination and builds collective
+					cohesion. Reading about successes gives hope that there is sanity in the world. The
+					movement is about building a better system, about showing it is possible. Protagonism
+					embraces life.
+				</p>
 			</div>
 			<div class="cardEight mosaicCard">
 				<h2>The Internet can be used to build social economic networks.</h2>
+				<p>
+					The Internet not only connects us all, but makes transfer of money possible. It makes it
+					possible to build economic strategy on a collective level, yet without the need for a
+					state or commerce as the agent. New Internationalism
+				</p>
 			</div>
-			<div class="cardNine mosaicCard"><h2>A hopeful narrative.</h2></div>
-			<div class="cardTen mosaicCard"><h2>Building a culture of the future.</h2></div>
+			<div class="cardNine mosaicCard">
+				<h2>A hopeful narrative.</h2>
+				<p>
+					There is so little hope for a better future amongst left networks, but hope can be
+					powerful. Being a part of a positive movement gives determination and builds collective
+					cohesion. Reading about successes gives hope that there is sanity in the world. The
+					movement is about building a better system, about showing it is possible. Protagonism
+					embraces life.
+				</p>
+			</div>
+			<div class="cardTen mosaicCard">
+				<h2>Building a culture of the future.</h2>
+				<p>
+					The quest that Friends in Common is on, could be described as "human liberation". We
+					think, that helping start up hundreds of collectives with social-ecological values can
+					have reverberations over time and over generations. Obviously, it's not really possible to
+					have a say on the nature of future human societies, but it is possible in the present day
+					to support communities, whose values are then more likely to be passed on to future
+					generations
+				</p>
+			</div>
 		</div>
 	</div>
 </div>
 <div class="sectionForm" id="sectionForm">
 	<h4 class="label">JOIN US</h4>
-	<h2>Big donations help a lot, small ones do too. 5e/m, hop!</h2>
+	<h2>Big donations help a lot,<br />small ones do too. 5e/m, hop!</h2>
 	<p>
 		Body text or body copy is the text forming the main content of a book, magazine, web page, or
 		any other printed or digital work. This is as a contrast to both additional components such as
 		headings, images, charts, footnotes etc. on each page, and also the pages of front matter that
 		form the introduction to a book.
 	</p>
+	<div class="sectionFormSubFrame">
+		<div class="donationForm">
+			<h4>MAKE A DONATION</h4>
+			<label for="name">First and Last Name</label>
+			<input type="text" placeholder="First and Last Name" />
+			<label for="email">Email Address</label>
+			<input type="text" placeholder="Email Address" />
+			<h4>I want to make a monthly donation of</h4>
+			<div class="formButtons">
+				<button class="one">1€</button>
+				<button class="five">5€</button>
+				<button class="ten">10€</button>
+				<button class="custom">custom amount</button>
+				<button class="submit">DONATE</button>
+			</div>
+		</div>
+	</div>
 </div>
 <div class="section2">
 	<h4 class="label">LABEL</h4>
@@ -117,12 +200,17 @@
 </div>
 
 <style lang="scss">
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+
 	:root {
 		--background-color-primary: #fff8e6;
 		--background-color-secondary: #f0e7cf;
 
 		--fic-gray: #3b3b3b;
-
+		--fic-gray-light: #868686;
 		--space-xxxs: 0.25rem; // 4px
 		--space-xxs: 0.375rem; // 6px
 		--space-xs: 0.5rem; // 8px
@@ -183,6 +271,7 @@
 	p {
 		font-family: 'Inter';
 		color: #3b3b3b;
+		font-size: 2rem;
 	}
 
 	h2 {
@@ -195,11 +284,22 @@
 	}
 
 	.btn {
+		text-decoration: none;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		border-radius: 100px;
 		min-height: 6vh;
 		min-width: 13vw;
 		font-family: 'Readex Pro';
 		font-size: 1.5rem;
+		transition: 0.4s;
+
+		&:hover {
+			background-color: var(--fic-gray-light);
+			color: var(--background-color-primary);
+			border: 0;
+		}
 	}
 
 	.btn-primary {
@@ -216,8 +316,8 @@
 
 	.x-padding {
 		@include media(tabletAndUp) {
-			padding-left: var(--space-xxxl);
-			padding-right: var(--space-xxxl);
+			padding-left: var(--space-xxxxl);
+			padding-right: var(--space-xxxxl);
 		}
 	}
 
@@ -240,10 +340,10 @@
 	.heroSection {
 		// height: auto;
 		background-color: var(--background-color-secondary);
-		min-height: auto;
+		// min-height: auto;
 
 		@include media(tabletAndUp) {
-			height: 100vh;
+			height: auto;
 			display: grid;
 			grid-template-columns: 1fr 75%;
 			grid-template-rows: 95px 1fr 1fr;
@@ -254,9 +354,6 @@
 
 			h2 {
 				font-size: 6.125rem;
-			}
-			p {
-				font-size: 1.5rem;
 			}
 		}
 	}
@@ -327,13 +424,15 @@
 		@extend .x-padding;
 		@extend .y-padding;
 		background-color: var(--background-color-secondary);
-		min-height: auto;
+		height: auto;
 
 		@include media(tabletAndUp) {
 			// The CSS for screens bigger than tablets
 			width: 100vw;
-			p {
-				font-size: 1.5rem;
+
+			.section1SubFrame {
+				display: flex;
+				column-gap: 50px;
 			}
 		}
 	}
@@ -353,7 +452,8 @@
 			background-color: var(--background-color-primary);
 			width: 80vw;
 			aspect-ratio: 1 / 1;
-			// padding-top: 100%;
+			margin-left: auto;
+			margin-right: auto;
 			display: grid;
 			grid-template-columns: 1fr 1fr 1fr;
 			grid-template-rows: 1fr 1fr 1fr 1fr;
@@ -375,6 +475,22 @@
 
 				container-type: size;
 				resize: both;
+
+				p {
+					display: none;
+				}
+
+				&:hover {
+					h2 {
+						display: none;
+					}
+
+					p {
+						display: block;
+						color: var(--fic-gray);
+						font-size: 6cqmin;
+					}
+				}
 			}
 
 			.cardOne {
@@ -390,7 +506,7 @@
 				grid-area: cardTwo;
 
 				h2 {
-					font-size: 22cqmin;
+					font-size: 23cqmin;
 				}
 			}
 			.cardThree {
@@ -446,7 +562,7 @@
 				grid-area: cardNine;
 
 				h2 {
-					font-size: 20cqmin;
+					font-size: 23cqmin;
 				}
 			}
 			.cardTen {
@@ -470,6 +586,100 @@
 			// The CSS for screens bigger than tablets
 			height: 100vh;
 			width: 100vw;
+		}
+
+		label {
+			font-family: 'Readex Pro';
+			color: var(--fic-gray);
+		}
+
+		input {
+			font-family: 'Readex Pro';
+			font-style: italic;
+			border-radius: 100px;
+			min-height: 6vh;
+			min-width: 13vw;
+			border: 1px solid var(--fic-gray);
+			background-color: var(--background-color-primary);
+		}
+
+		.sectionFormSubFrame {
+			display: flex;
+			justify-content: flex-end;
+		}
+
+		.donationForm {
+			@extend .flex-column-row-gap;
+			width: 25vw;
+
+			.formButtons {
+				display: grid;
+				grid-template-columns: 1fr 1fr 1fr;
+				grid-template-rows: 1fr 1fr;
+				column-gap: var(--space-l);
+				row-gap: var(--space-m);
+				grid-template-areas:
+					'one five ten'
+					'custom submit submit';
+
+				.one {
+					grid-area: one;
+					border-radius: 100px;
+					min-height: 6vh;
+					border: 1px solid var(--fic-gray);
+					background-color: var(--background-color-primary);
+					font-family: 'Readex Pro';
+					font-size: 1.5rem;
+				}
+
+				.five {
+					grid-area: five;
+					border-radius: 100px;
+					min-height: 6vh;
+					border: 1px solid var(--fic-gray);
+					background-color: var(--background-color-primary);
+					font-family: 'Readex Pro';
+					font-size: 1.5rem;
+				}
+
+				.ten {
+					grid-area: ten;
+					border-radius: 100px;
+					min-height: 6vh;
+					border: 1px solid var(--fic-gray);
+					background-color: var(--background-color-primary);
+					font-family: 'Readex Pro';
+					font-size: 1.5rem;
+				}
+
+				.custom {
+					grid-area: custom;
+					border-radius: 100px;
+					min-height: 6vh;
+					border: 1px solid var(--fic-gray);
+					background-color: var(--background-color-primary);
+					font-family: 'Readex Pro';
+					font-size: 1.5rem;
+				}
+
+				.submit {
+					grid-area: submit;
+					border-radius: 100px;
+					min-height: 6vh;
+					border: 1px solid var(--fic-gray);
+					background-color: var(--fic-gray);
+					transition: 0.4s;
+					font-size: 1.5rem;
+					font-family: 'Readex Pro';
+					color: var(--background-color-primary);
+
+					&:hover {
+						background-color: var(--fic-gray-light);
+						color: var(--background-color-primary);
+						border: 0;
+					}
+				}
+			}
 		}
 	}
 
