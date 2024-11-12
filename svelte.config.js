c33435dd74172eb10bc24c0182d13210958358fe
pessi-v <pessimo@posteo.net>
2024-11-12 16:17:28 +0100

    try without scss: true

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,6 @@ import { sveltePreprocess } from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: sveltePreprocess({
-		scss: true,
 		prependData: `@import 'src/app.scss';`
 	}),
 	kit: {
